74aacec791449373b1f09aee77d5dc1d74ec970b
Alexey Tsvetkov <cai.cwet@gmail.com>
2019-09-01 18:28:55 +0300

    refactor air.js, ball.js

--- src/model/ball.js
+++ src/model/ball.js
@@ -8,8 +8,8 @@ export default class Ball extends Model {
     super({ kind: 'ball' });
     this.x = x;
     this.y = y;
-    this.x1 = x + BALL_R;
-    this.y1 = y + BALL_R;
+    this.x1 = this.x + BALL_R;
+    this.y1 = this.y + BALL_R;
     this.speed = { x: 12, y: 18 };
   }
 
@@ -29,8 +29,5 @@ export default class Ball extends Model {
     if (this.y === PLAYGROUND_HEIGHT - BALL_R || this.y === 0) {
       this.speed = { ...this.speed, y: -this.speed.y };
     }
-
-    this.x1 = this.x + BALL_R;
-    this.y1 = this.y + BALL_R;
   }
 }
